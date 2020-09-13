306e1bd656f6b1ae84df0452744c2c9df40522b0
omkarRJoshi <omkarjoshi9545@gmail.com>
2020-09-13 14:36:07 +0530

    add dependecies for spark

---
+++ fraudDetection/src/main/java/com/geca/proect/fraudDetection/App.java
@@ -0,0 +1,13 @@
+package com.geca.proect.fraudDetection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
